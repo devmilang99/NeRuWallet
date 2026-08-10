20a8e5d739a50e3c027ffb4d37d0cb5911326015
MilanGhimire <devmilang99@gmail.com>
2026-08-10 12:38:44 +0545

    changes: GitHub fixed and optimization made for iOS
    Signed-off-by: MilanGhimire <devmilang99@gmail.com>

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -69,8 +69,8 @@ import UIKit
                                        details: nil))
                     return
                 }
-                let processed = self?.rustSigner.processTransactionData(data: data.data)
-                result(FlutterStandardTypedData(bytes: processed ?? Data()))
+                let processed = self?.rustSigner.processTransactionData(data: [UInt8](data.data))
+                result(FlutterStandardTypedData(bytes: Data(processed ?? [])))
 
             case "verifyRustSignature":
                 guard let args = call.arguments as? [String: Any],
@@ -82,9 +82,9 @@ import UIKit
                                        details: nil))
                     return
                 }
-                let isValid = self?.rustSigner.verifySignature(publicKey: pubKey.data,
-                                                               message: msg.data,
-                                                               signature: sig.data)
+                let isValid = self?.rustSigner.verifySignature(publicKey: [UInt8](pubKey.data),
+                                                               message: [UInt8](msg.data),
+                                                               signature: [UInt8](sig.data))
                 result(isValid)
 
             default:
